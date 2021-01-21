7d53e53d39e50eabd6202095924bff7ba6a9fab3
mikili1995 <abuljevic8@gmail.com>
2021-01-21 22:58:07 +0100

    First version of search functionality

--- TicketsSale/src/services/ManifestationService.java
+++ TicketsSale/src/services/ManifestationService.java
@@ -1,11 +1,17 @@
 package services;
 
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,12 +20,13 @@ import javax.ws.rs.core.MediaType;
 
 
 import model.Manifestation;
+import model.QueryParams;
 import dao.LocationDAO;
 import dao.ManifestationDAO;
 
 @Path("/manifestations")
 public class ManifestationService {
-
+	
 	@Context
 	ServletContext ctx;
 	LocationDAO locationDAO;
@@ -52,8 +59,93 @@ public class ManifestationService {
 	@Path("/{name: .+}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Manifestation getManifestation(@PathParam("name") String name) {
+		
 		ManifestationDAO dao = (ManifestationDAO) ctx.getAttribute("ManifestationDAO");
 		return dao.find(name);
+	}
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<Manifestation> getSearchedManifestations(QueryParams params) {
+		
+		if(!validateParams(params)) {
+			return new ArrayList<Manifestation>();
+		}
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		ManifestationDAO dao = (ManifestationDAO) ctx.getAttribute("ManifestationDAO");
+		
+		Collection<Manifestation> collection = dao.findAll();
+				
+		if(!params.getName().equals("")) {
+			collection = collection.stream()
+				.filter(manifestation -> manifestation.getName().toLowerCase().startsWith(params.getName().toLowerCase())).collect(Collectors.toList());
+		}
+		
+		if(!params.getLocation().equals("")) {
+			collection = collection.stream()
+				.filter(manifestation -> manifestation.getLocation().getAddress().toLowerCase().contains(params.getLocation().toLowerCase())).collect(Collectors.toList());
+		}
+		if(!params.getPriceFrom().equals("")) {
+			collection = collection.stream()
+				.filter(manifestation -> manifestation.getPriceOfRegularTicket() > Double.parseDouble(params.getPriceFrom())).collect(Collectors.toList());
+		}
+		
+		if(!params.getPriceUntil().equals("")) {
+			collection = collection.stream()
+					.filter(manifestation -> manifestation.getPriceOfRegularTicket() < Double.parseDouble(params.getPriceUntil())).collect(Collectors.toList());
+		}
+		
+		if(!params.getDateFrom().equals("")) {	
+			try {
+				final Date d = sdf.parse(params.getDateFrom());
+				collection = collection.stream()
+						.filter(manifestation -> manifestation.getDate().after(d)).collect(Collectors.toList());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		
+		}
+		
+		if(!params.getDateUntil().equals("")) {
+			try {
+				final Date d = sdf.parse(params.getDateUntil());
+				collection = collection.stream()
+						.filter(manifestation -> manifestation.getDate().before(d)).collect(Collectors.toList());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return collection;
+	}
+	
+	private boolean validateParams(QueryParams params) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			double num;
+			if(!params.getPriceFrom().equals("")) {
+				num = Double.parseDouble(params.getPriceFrom());
+			}
+			if(!params.getPriceUntil().equals("")) {
+				num = Double.parseDouble(params.getPriceUntil());
+			}
+			
+			Date d;
+			if(!params.getDateFrom().equals("")) {
+				d = sdf.parse(params.getDateFrom());
+			}
+			if(!params.getDateFrom().equals("")) {
+				d = sdf.parse(params.getDateFrom());
+			}				
+		} catch (Exception e) {
+			return false;
+		}
+		
+		return true;
 	}
 	
 	/*@POST
